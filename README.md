55a4dba3422746ffceb3a8cd38c362c2da1a4145
Lukas Huber <hubernikus@gmail.com>
2022-05-10 19:55:35 +0200

    update setup file and instructions

--- README.md
+++ README.md
@@ -10,6 +10,25 @@ Go to file directory:
 cd motion_learning_direction_space 
 ``` 
 
+# Setup 
+Make sure you use python version 3.10, e.g., setup with virtual environment:
+
+``` bash
+python3.10 -m venv .venv
+```
+
+Activate the environment
+``` bash
+source .venv/bin/activate
+```
+
+## Install Packages
+Setup environment and install packages
+``` bash
+pip install -e . && pip install -r requirments.txt
+```
+(TODO: include installing of the requirements in `setup.py`)
+
 
 ## Error Handling
 You've got missing modules? Maybe you forgot to add the submodules, add them with:
--- setup.py
+++ setup.py
@@ -2,13 +2,14 @@
 
 from setuptools import setup
 
-setup(name='motion_learning_direction_space',
-      version='1.0',
-      description='Motion Learning in Direction Space',
-      author='Lukas Huber',
-      author_email='lukas.huber@epfl.ch',
-      packages=['motion_learning_direction_space',
-                'motion_learning_direction_space.example_folder'],
-      scripts=[],
-      package_dir={'': 'src'}
-     )
+setup(
+    name='motion_learning_direction_space',
+    version='0.0.1',
+    description='Motion Learning in Direction Space',
+    author='Lukas Huber',
+    author_email='lukas.huber@epfl.ch',
+    packages=['motion_learning_direction_space',
+              'motion_learning_direction_space.example_folder'],
+    scripts=[],
+    package_dir={'': 'src'}
+)
